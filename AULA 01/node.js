778d364b1ee5afe803786668a4d8a9a0c10ceb83
júlia <julia.pardim@icloud.com>
2026-09-18 10:13:41 -0300

    AULA BACKEND

---
+++ AULA 01/node.js
@@ -0,0 +1,6 @@
+
+// Variáveis
+// Variables são contêineres que armazenam valores. Você começa declarando uma variável com a palavra-chave let, seguida do nome que você dá à variável:
+
+// let myVariable;
+// Um ponto e vírgula no final de uma linha indica onde uma instrução termina. Só é necessário quando você precisa separar instruções em uma única linha. 

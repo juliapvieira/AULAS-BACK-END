9bb15206fdea65bf9cd3ae073252475663b8ab45
júlia <julia.pardim@icloud.com>
2026-09-18 11:07:00 -0300

    EXERCICIO

--- AULA 02/conceito_tipos_de_dados.js
+++ AULA 02/conceito_tipos_de_dados.js
@@ -9,3 +9,27 @@
 //Array (Listas de valores)
 //NULL (Valor vazio)
 //Undefined (quando algo não foi definido.
+
+
+//STRING (Textos)
+//Uma string é um texto, sempre escrito entre aspas ("" ou '')
+let nome = "Jarvis"
+let mensagem = 'Olá, mundo"'
+
+console.log(nome);
+console.log(mensagem);
+
+let saudacao = "Olá, " + nome + "!";
+console.log(saudacao); //Exibe olá jarvis
+
+
+//typeof
+//typeof serve para descobrir o tipo de um valor ou variável 
+
+let nomeDois = "Júlia"
+console.log(typeof nomeDois);
+
+let soma = 10 + 5;
+console.log(soma);
+
+
---
+++ AULA 02/exercicio_01.js
@@ -0,0 +1,11 @@
+// Atividade Number
+//Crie uma variável chamada anoAtual e outra anoNascimento
+//Calcule e exiba a idade no console
+
+let anoAtual = 2026;
+let anoNascimento = 2010;
+console.log(anoAtual);
+console.log(anoNascimento);
+
+let idade = anoAtual - anoNascimento;
+console.log(idade);
